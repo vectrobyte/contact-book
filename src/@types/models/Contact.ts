a3ef42a90d581bcdab6fad606d5dbb49a7c77a2d
Dilip Bhattarai <vectrobyte@gmail.com>
2023-04-21 12:53:40 +0530

    feat: design contacts page and use the hook to list contacts into contacts table

--- src/@types/models/Contact.ts
+++ src/@types/models/Contact.ts
@@ -1,5 +1,5 @@
 export type Contact = {
-  id: string | number;
+  id: string;
   full_name: string;
   email?: string;
   phone: string;
---
+++ src/pages/contacts/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { MdAdd, MdDelete, MdEdit } from 'react-icons/md';
+
+import Button from '@/components/buttons/Button';
+import IconButton from '@/components/buttons/IconButton';
+import AppLayout from '@/layouts/app-layout/AppLayout';
+import { useGetContacts } from '@/lib/context/data/useGetContacts';
+
+type ContactsProps = React.HTMLAttributes<HTMLElement>;
+
+const Contacts: React.FC<ContactsProps> = () => {
+  const { contacts } = useGetContacts();
+
+  return (
+    <AppLayout>
+      <table className="w-full">
+        <thead>
+          <tr>
+            <td className="border-b p-3 text-gray-600">Name</td>
+            <td className="border-b p-3 text-gray-600">Email</td>
+            <td className="border-b p-3 text-gray-600">Mobile</td>
+            <td className="border-b p-3 text-gray-600" />
+          </tr>
+        </thead>
+
+        <tbody>
+          {contacts.length
+            ? contacts.map((contact) => (
+                <tr className="transition-colors hover:bg-gray-50" key={contact.id}>
+                  <td className="w-1/3 p-2 text-sm">{contact.full_name}</td>
+                  <td className="p-2 text-sm font-light">{contact.email}</td>
+                  <td className="p-2 text-sm font-light">{contact.phone}</td>
+                  <td className="w-[100px] py-2">
+                    <div className="flex items-center justify-end gap-1">
+                      <IconButton title="Edit">
+                        <MdEdit />
+                      </IconButton>
+                      <IconButton title="Delete">
+                        <MdDelete />
+                      </IconButton>
+                    </div>
+                  </td>
+                </tr>
+              ))
+            : ''}
+        </tbody>
+      </table>
+
+      <div className="fixed bottom-10 right-10">
+        <Button className="rounded-full bg-gray-50">
+          <div className="flex items-center justify-center gap-2">
+            <MdAdd size={24} />
+            <span>Create Contact</span>
+          </div>
+        </Button>
+      </div>
+    </AppLayout>
+  );
+};
+
+export default Contacts;
