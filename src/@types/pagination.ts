2b8d24e36f57060f78afa2ac8ad52e508329b29b
Dilip Bhattarai <vectrobyte@gmail.com>
2023-04-22 00:40:39 +0530

    feat: re-write the prisma schema and create apis for performing contact CRUD operations

---
+++ src/@types/pagination.ts
@@ -0,0 +1,18 @@
+export type PaginationMeta = {
+  total: number;
+  total_page: number;
+  current_page: number;
+  per_page: number;
+  current_page_size: number;
+};
+
+export type PageParams = Partial<{
+  page: number;
+  size: number;
+  keyword?: string;
+}>;
+
+export type PaginatedResult<T> = {
+  data: T[];
+  meta: PaginationMeta;
+};
