aea3d748e4e4a5d11046f3f71ed12f772f8a8b6b
Dilip Bhattarai <vectrobyte@gmail.com>
2023-04-26 19:08:43 +0530

    feat: add auth middleware in delete api

--- src/pages/api/contacts/delete.ts
+++ src/pages/api/contacts/delete.ts
@@ -1,7 +1,10 @@
 import { IdSchema } from '@/lib/schemas/common.schema';
 import { Delete } from '@/server/handlers/route.handler';
+import { AuthMiddleware } from '@/server/middleware/auth.middleware';
 import { deleteContact } from '@/server/services/contact.service';
 
-export default Delete(IdSchema, async ({ id }) => {
-  return deleteContact(id);
-});
+export default AuthMiddleware(
+  Delete(IdSchema, async ({ id }) => {
+    return deleteContact(id);
+  })
+);
