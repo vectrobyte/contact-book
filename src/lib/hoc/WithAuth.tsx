fb727eb60c6846f95029fe8ed68988129519c562
Dilip Bhattarai <vectrobyte@gmail.com>
2023-05-08 13:12:49 +0530

    feat: add loader and update the auth check with useSession hook

--- src/lib/hoc/WithAuth.tsx
+++ src/lib/hoc/WithAuth.tsx
@@ -1,28 +1,37 @@
+import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { getSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useEffect } from 'react';
 
+import { useIsDesktop } from '@/lib/hooks/useIsDesktop';
 import { ROUTE_PATHS } from '@/routes';
 
 export default function WithAuth(Component) {
   return function WithAuth(props) {
+    const isDesktop = useIsDesktop();
     const router = useRouter();
-    const [loading, setLoading] = useState(true);
+    const { status } = useSession();
 
     useEffect(() => {
-      async function checkAuth() {
-        const session = await getSession();
-        setLoading(false);
-        if (!session) {
+      return () => {
+        if (status === 'unauthenticated') {
           void router.push(ROUTE_PATHS.home);
         }
-      }
+      };
+    }, [router, status]);
 
-      void checkAuth();
-    }, [router]);
-
-    if (loading) {
-      return <></>;
+    if (status === 'loading') {
+      return (
+        <div className="flex-center flex-col gap-5 px-16 py-32 sm:gap-10 md:px-32 lg:p-64">
+          <Image
+            src="/logo.svg"
+            alt=""
+            height={isDesktop ? 150 : 100}
+            width={isDesktop ? 150 : 100}
+            className="mt-[130px] flex-shrink-0 animate-pulse opacity-10 grayscale"
+          />
+        </div>
+      );
     }
 
     return <Component {...props} />;
